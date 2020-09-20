99e965fb44494632d2caaccfd4984d3e1614bdf6
Dominic Hopton <d.hopton@codevoid.net>
2020-09-20 14:11:51 -0700

    Add Basic Forward/Back Navigation + Debug Buttons
    This adds two meaningful areas of functionality:
    1. Backward navigation from system controls (UI, and Win+Backspace) + Mouse button
    2. Debug Button placeholders that show on Ctrl to allow navigation to test
    pages separately from the experience

--- App/MainUI.cpp
+++ App/MainUI.cpp
@@ -3,7 +3,11 @@
 #include "MainUI.g.cpp"
 
 using namespace winrt;
+using namespace Windows::UI::Core;
+using namespace Windows::UI::Input;
 using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Navigation;
 using namespace Windows::UI::Xaml::Interop;
 
 namespace winrt::Codevoid::MusicHall::implementation
@@ -11,10 +15,96 @@ namespace winrt::Codevoid::MusicHall::implementation
     MainUI::MainUI()
     {
         InitializeComponent();
+        
+        // Attach global handlers to the Core Window so we can toggle the debug
+        // buttons visibility when control is held
+        auto currentWindow = Window::Current().CoreWindow();
+        m_coreWindowKeyDownRevoker = currentWindow.KeyDown(winrt::auto_revoke, { this, &MainUI::HandleWindowKeyDown });
+        m_coreWindowKeyUpRevoker = currentWindow.KeyUp(winrt::auto_revoke, { this, &MainUI::HandleWindowKeyUp });
+        m_coreWindowPointerReleasedRevoker = currentWindow.PointerReleased(winrt::auto_revoke, { this, &MainUI::HandlePointerReleased });
+
+        m_navigationManager = SystemNavigationManager::GetForCurrentView();
+        m_navigationManager.AppViewBackButtonVisibility(AppViewBackButtonVisibility::Collapsed);
+        m_navigationManagerBackRequestedRevoker = m_navigationManager.BackRequested(winrt::auto_revoke, { this, &MainUI::HandleBackRequested });
+    }
+
+    void MainUI::HandlePlaceholderButtonClick(IInspectable const& source, RoutedEventArgs const& /*args*/)
+    {
+        auto button = source.try_as<ButtonBase>();
+        if (button == nullptr)
+        {
+            return;
+        }
+
+        this->NavigateToPlaceholderPage(unbox_value_or(button.Tag(), L"Unknown"));
+    }
+
+    void MainUI::HandleFrameNavigation(IInspectable const& /*source*/, NavigationEventArgs const& /*args*/)
+    {
+        if (this->NavigationFrame().CanGoBack())
+        {
+            m_navigationManager.AppViewBackButtonVisibility(AppViewBackButtonVisibility::Visible);
+        }
+        else
+        {
+            m_navigationManager.AppViewBackButtonVisibility(AppViewBackButtonVisibility::Collapsed);
+        }
     }
 
     void MainUI::Navigate(TypeName const& pageType, IInspectable const& parameter)
     {
         this->NavigationFrame().Navigate(pageType, parameter);
+    }
+
+    void MainUI::HandleWindowKeyDown(CoreWindow const& /*sender*/, KeyEventArgs const& /*args*/)
+    {
+        VisualStateManager::GoToState(*this, L"ShowDebugButtons", false);
+    }
+
+    void MainUI::HandleWindowKeyUp(CoreWindow const& /*sender*/, KeyEventArgs const& /*args*/)
+    {
+        VisualStateManager::GoToState(*this, L"HideDebugButtons", false);
+    }
+
+    void MainUI::HandlePointerReleased(CoreWindow const& /*sender*/, PointerEventArgs const& args)
+    {
+        // We want to handle the button on release of the back button, not
+        // when it's pressed down. This mimics the common behaviour in browsers
+        auto pointerProperties = args.CurrentPoint().Properties();
+        if (pointerProperties.PointerUpdateKind() != PointerUpdateKind::XButton1Released)
+        {
+            return;
+        }
+
+        args.Handled(true);
+        this->NavigateBackIfPossible();
+    }
+
+    void MainUI::HandleBackRequested(IInspectable const&, BackRequestedEventArgs const& args)
+    {
+        // Make sure the system sees that we handled it, so that it doesn't
+        // also invoke the system handler (E.g. in tablet mode go back to the
+        // previous app)
+        args.Handled(this->NavigateBackIfPossible());
+    }
+
+    bool MainUI::NavigateBackIfPossible()
+    {
+        if (!this->NavigationFrame().CanGoBack())
+        {
+            return false;
+        }
+
+        this->NavigationFrame().GoBack();
+        return true;
+    }
+
+    void MainUI::NavigateToPlaceholderPage(hstring const& placeholderText)
+    {
+        Interop::TypeName pageType;
+        pageType.Name = L"Codevoid.MusicHall.PlaceholderPage";
+        pageType.Kind = Interop::TypeKind::Primitive;
+
+        this->Navigate(pageType, box_value(placeholderText));
     }
 }
--- App/MainUI.h
+++ App/MainUI.h
@@ -1,6 +1,8 @@
 ﻿#pragma once
 
 #include "MainUI.g.h"
+#include <winrt/Windows.UI.Core.h>
+#include <winrt/Windows.UI.Input.h>
 
 namespace winrt::Codevoid::MusicHall::implementation
 {
@@ -8,7 +10,46 @@ namespace winrt::Codevoid::MusicHall::implementation
     {
         MainUI();
 
+        /// <summary>
+        /// API for navigating to a supplied page type, with the supplied
+        /// parameter information.
+        /// </summary>
+        /// <param name="pageType">Type of page to navigate to</param>
+        /// <param name="parameter"
+        /// Parameter to pass to the page. The type of this parameter is
+        /// defined by the page itself. Consult the page your navigating to
+        /// to understand what parameters are supported.
+        /// </param>
         void Navigate(Windows::UI::Xaml::Interop::TypeName const& pageType, Windows::Foundation::IInspectable const& parameter);
+        void HandlePlaceholderButtonClick(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+        void HandleFrameNavigation(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::Navigation::NavigationEventArgs const& args);
+
+        private:
+            void HandleWindowKeyDown(Windows::UI::Core::CoreWindow const& sender, Windows::UI::Core::KeyEventArgs const& args);
+            void HandleWindowKeyUp(Windows::UI::Core::CoreWindow const& sender, Windows::UI::Core::KeyEventArgs const& args);
+            void HandleBackRequested(Windows::Foundation::IInspectable const& sender, Windows::UI::Core::BackRequestedEventArgs const& args);
+            void HandlePointerReleased(Windows::UI::Core::CoreWindow const& sender, Windows::UI::Core::PointerEventArgs const& args);
+
+            /// <summary>
+            /// Helper to navigate to a placeholder page with some placeholder
+            /// text. Used for simple testing.
+            /// </summary>
+            /// <param name="placeholderText">Text to display on the page</param>
+            void NavigateToPlaceholderPage(winrt::hstring const& placeholderText);
+
+            /// <summary>
+            /// Navigates the main UI frame back one item, if there is something
+            /// to navigate back to to.
+            /// </summary>
+            /// <returns>True if back navigation was possible; false otherwise</returns>
+            bool NavigateBackIfPossible();
+
+            // Event revoker dumpster fields
+            Windows::UI::Core::SystemNavigationManager m_navigationManager{ nullptr };
+            winrt::Windows::UI::Core::CoreWindow::KeyDown_revoker m_coreWindowKeyDownRevoker;
+            winrt::Windows::UI::Core::CoreWindow::KeyUp_revoker m_coreWindowKeyUpRevoker;
+            winrt::Windows::UI::Core::CoreWindow::PointerReleased_revoker m_coreWindowPointerReleasedRevoker;
+            winrt::Windows::UI::Core::SystemNavigationManager::BackRequested_revoker m_navigationManagerBackRequestedRevoker;
     };
 }
 
